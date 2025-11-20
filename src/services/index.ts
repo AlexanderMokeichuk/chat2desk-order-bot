12d0c498c77d832ed69415bdf1b87909da4ad744
alex <alexander.q.qa.a@mail.ru>
2025-11-20 16:42:45 +0600

    feat(services): add order service to barrel export

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,3 +1,5 @@
 export * from './state/state.service';
 export * from './chat2desk/chat2desk.service';
 export * from './chat2desk/rate-limiter.service';
+export * from './order/order.service';
+export * from './order/order.repository';
